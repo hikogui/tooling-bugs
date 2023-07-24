ed148b4734e4e0684f80eb817531b044c2e9fdb4
Take Vos <info@takevos.nl>
2023-07-24 17:56:07 +0200

    Cleanup

--- msvc_module_with_atomic_header/example.cpp
+++ msvc_module_with_atomic_header/example.cpp
@@ -1,8 +1,14 @@
 
 #include "example.hpp"
+
+// It seems the problem "error C2953: 'std::conditional': class template has already been defined" is caused by:
+//  - Including a system, in this case `<type_traits>`, after;
+//  - Defining a function that uses the functionality from an imported module, in this case `weird_message`;
+//  - That module's functionality itself depends on class-template from a system header, in this case `std::atomic<>`.
 #include <type_traits>
 
-int main()
-{
-    return test();
-}
+// The following function has nothing to do with the bug.
+//int main()
+//{
+//    return std::is_const_v<int>;
+//}
